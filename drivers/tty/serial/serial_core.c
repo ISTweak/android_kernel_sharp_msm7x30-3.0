a5ae055367ff1994c094f5b9e149a5ddc4b7d56a
Marijuana <istweak@d-o-b.org>
2014-09-27 23:33:10 +0900

    Linux Kernel to 3.0.77

--- drivers/tty/serial/serial_core.c
+++ drivers/tty/serial/serial_core.c
@@ -1991,6 +1991,8 @@ int uart_suspend_port(struct uart_driver *drv, struct uart_port *uport)
 		mutex_unlock(&port->mutex);
 		return 0;
 	}
+	put_device(tty_dev);
+
 	if (console_suspend_enabled || !uart_console(uport))
 		uport->suspended = 1;
 
@@ -2056,9 +2058,11 @@ int uart_resume_port(struct uart_driver *drv, struct uart_port *uport)
 			disable_irq_wake(uport->irq);
 			uport->irq_wake = 0;
 		}
+		put_device(tty_dev);
 		mutex_unlock(&port->mutex);
 		return 0;
 	}
+	put_device(tty_dev);
 	uport->suspended = 0;
 
 	/*
